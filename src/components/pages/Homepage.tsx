1e1a3a15fbb2f217c1294ea781088bd220bf597d
Zaka Urrehman Shah <114782819+zaka-urrehman@users.noreply.github.com>
2024-12-16 02:12:00 +0500

    add Pricing and Tagline Section

--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -1,12 +1,18 @@
 
 import Contact from "../shared/Contact";
+import { Features } from "../shared/Features";
 import Hero from "../shared/Hero";
+import { Pricing } from "../shared/Pricing";
+import { Tagline } from "../shared/Tagline";
 
 export default function Homepage() {
     return (
         <div>
             <Hero/>
-            <Contact/>                       
+            <Features/>                   
+            <Tagline/>   
+            <Pricing/>
+            <Contact/> 
         </div>
     )
 }
