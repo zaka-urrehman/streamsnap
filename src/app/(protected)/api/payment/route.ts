ccb2c170122cb70777ae2090e54de511f73e74fb
Zaka Urrehman Shah <114782819+zaka-urrehman@users.noreply.github.com>
2025-01-17 07:40:48 +0500

    feat: add stripe

---
+++ src/app/(protected)/api/payment/route.ts
@@ -0,0 +1,39 @@
+import { stripe } from '@/lib/stripe';
+import { currentUser } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    try {
+        const user = await currentUser();
+        if (!user) {
+            return NextResponse.json({ status: 404, message: 'User not found' });
+        }
+
+        const priceId = process.env.STRIPE_SUBSCRIPTION_PRICE_ID;
+        if (!priceId) {
+            return NextResponse.json({ status: 500, message: 'Price ID not configured' });
+        }
+
+        const session = await stripe.checkout.sessions.create({
+            mode: 'subscription',
+            line_items: [
+                {
+                    price: priceId,
+                    quantity: 1,
+                },
+            ],
+            success_url: `${process.env.NEXT_PUBLIC_HOST_URL}/payment?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.NEXT_PUBLIC_HOST_URL}/payment?cancel=true`,
+        });
+
+        if (session) {
+            return NextResponse.json({
+                status: 200,
+                session_url: session.url,
+            })
+        }
+    } catch (error) {
+        console.error('Error creating Stripe session:', error);
+        return NextResponse.json({ status: 500, message: 'Internal Server Error' });
+    }
+}
