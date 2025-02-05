902f7260c76d2ef4844094e0fe2bcf599cf42c83
Zaka Urrehman Shah <114782819+zaka-urrehman@users.noreply.github.com>
2025-02-05 08:40:40 +0500

    fix: allow connecting instagram account

--- src/app/(protected)/dashboard/[slug]/integrations/IntegrationCard.tsx
+++ src/app/(protected)/dashboard/[slug]/integrations/IntegrationCard.tsx
@@ -1,7 +1,8 @@
 'use client'
+import { onOAuthInstagram } from '@/actions/integration'
 // import { onOAuthInstagram } from '@/actions/integrations'
-// import { onUserInfo } from '@/actions/user'
-// import { useQuery } from '@tanstack/react-query'
+import { getUserInfo } from '@/actions/user'
+import { useQuery } from '@tanstack/react-query'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
@@ -14,33 +15,38 @@ type Props = {
 
 const IntegrationCard = ({ description, icon, strategy, title }: Props) => {
 
-    // const onInstaOAuth = () => onOAuthInstagram(strategy)
+    const onInstaOAuth = () => {
+        const call = onOAuthInstagram(strategy)
+        console.log("call", call)
+        return call
+    }
 
-    // const { data } = useQuery({
-    //     queryKey: ['user-profile'],
-    //     queryFn: onUserInfo,
-    // })
+    const { data } = useQuery({
+        queryKey: ['user-profile'],
+        queryFn: getUserInfo,
+    })
 
-    // const integrated = data?.data?.integrations.find(
-    //     (integration) => integration.name === strategy
-    // )
+    // @ts-ignore
+    const integrated = data?.data?.integrations.find(
+        (integration: any) => integration.name === strategy
+    )
+
 
     return (
         <div className="border-2 border-gray-400 rounded-2xl p-5 flex max-md:flex-col max-md:text-center items-center justify-between gap-y-3 gap-x-5 ">
             {/* <div> */}
-                {icon}
+            {icon}
             {/* </div> */}
             <div className="flex flex-col flex-1">
                 <h3 className="lg:text-lg"> {title}</h3>
                 <p className="text-[#9D9D9D] text-sm lg:text-base ">{description}</p>
             </div>
             <Button
-                // onClick={onInstaOAuth}
-                // disabled={integrated?.name === strategy}
+                onClick={onInstaOAuth}
+                disabled={integrated?.name === strategy}
                 className=" text-white rounded-full  bg-mySecondary-500  hover:opacity-70 transition duration-100 max-md:mt-3"
             >
-                {/* {integrated ? 'Connected' : 'Connect'} */}
-                Connect
+                {integrated ? 'Connected' : 'Connect'}
             </Button>
         </div>
     )
